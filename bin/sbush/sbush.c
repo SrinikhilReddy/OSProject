924cc14c658c48b128da89a502135a6aa7f1ea37
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-01 12:50:38 -0400

    Implemented chdir, method to run builtin shell commands

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void readInput();
 void parseInput();
-//char *commands[] = { "cd","pipe" }
+void execCommand();
+void chdir_1(char **args);
+char *commands[] = { "cd" };
 char *in ; //= malloc(sizeof(char)*1000);
 char **args; //=malloc(sizeof(char)*1000*1000);
 char *command;
@@ -24,6 +27,14 @@ int main(int argc, char *argv[], char *envp[]) {
  	return 0;
 }
 
+void execCommand(){
+	for(int i=0;i<(sizeof(commands)/sizeof(char));i++){	
+//Need better if checks
+		if( (strcmp(command,commands[i]) == 0) && (strcmp(command,"cd") == 0)){
+			chdir_1(args);	
+		}
+	}
+}
 void readInput(){
 	char c;
 	int i = 0;
@@ -44,4 +55,9 @@ void parseInput(){
 		args[i] = temp;
 		temp = strtok(NULL," ");
 	}
+}
+
+void chdir_1(char **args){
+	// Error cases to be handled	
+	chdir(args[0]);
 }
