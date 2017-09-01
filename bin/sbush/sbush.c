b60ddbcf97f2093aa7c4e820a419d6b1655fd6db
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-01 02:07:38 -0400

    Changes from master. Ferdman's edits in make file

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 void readInput();
+void parseInput();
+//char *commands[] = { "cd","pipe" }
 char *in ; //= malloc(sizeof(char)*1000);
+char **args; //=malloc(sizeof(char)*1000*1000);
+char *command;
 
-int main(int argc, char* argv[]) {
-  	puts("sbush> ");
-	in = malloc(sizeof(char)*1000); 	
-	readInput();
-	
+//int main(int argc, char* argv[]) {
+int main(int argc, char *argv[], char *envp[]) { 
+ 	while(1){
+		puts("sbush> ");
+		in = malloc(sizeof(char)*1000); 	
+		readInput();
+		if(strcmp(in,"exit") == 0){
+			puts("Exiting.....");
+			break;	
+		}
+	}
+	free(in);
+//	free(args);
+//	free(command);
 	//	puts(in);
  	 return 0;
 }
