e5c5a6852014629af9a2eaec48dfccc6252c9099
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-01 00:04:13 -0400

    Intial commit. Basic code for sbush to read input

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void readInput();
+char *in ; //= malloc(sizeof(char)*1000);
 
 int main(int argc, char* argv[]) {
-  puts("sbush> ");
-  return 0;
+  	puts("sbush> ");
+	in = malloc(sizeof(char)*1000); 	
+	readInput();
+	
+	//	puts(in);
+ 	 return 0;
+}
+
+void readInput(){
+	char c;
+	int i = 0;
+	while( (c=getchar())!= '\n'){
+		in[i] = c;
+		i++;
+	}
+}
+
+void parseInput(){
+	
 }
