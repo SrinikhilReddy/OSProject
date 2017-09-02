2faf34dd660ba818a08f9e88df9e57046c45a38e
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-02 01:18:26 -0400

    Changes for non-interactive shell scripting. Handled few seg faults

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -20,49 +20,49 @@ int main(int argc, char *argv[], char *envp[]) {
 			in = malloc(sizeof(char)*1000); 	
 			readInput();
 			if(strcmp(in,"exit") == 0){
-				puts("Exiting.....");
-				break;	
+				return 0;	
 			}
 			parseInput();
 			execCommand();
 		}
 	}else{
-		puts(argv[1]);
 		FILE *fp;
-		//in = malloc(sizeof(char)*1000);
 		fp = fopen(argv[1],"r");
-		int c = 0,i=0;
-		//c = getc(fp);
+		if(fp == NULL){
+			puts("File not found, Exiting!");
+			return 0;
+		}
+		int c,i=0;
 		while(c!=EOF){
-		        puts("sss");
-			in = malloc(sizeof(char)*1000);
-			c = getc(fp++);	
-			while(c!='\n'){
+			in = malloc(1000*sizeof(char));
+			c = fgetc(fp);	
+			while(c!='\n' && c!=EOF){
 				in[i++]=(char) c;
-				puts(in);
-				c = getc(fp++);
-				puts(in);	
-			}
-				
+				c = fgetc(fp);
+			}	
 			puts(in);
+			parseInput();
+			execCommand();			
 			i=0;
-			free(in);
-		//c=getc(++fp);
 		}
 	}
-	
 	free(in);
- 	
+	free(args);
+	free(command); 	
 	return 0;
 }
 
 void execCommand(){
-	puts(command);
+//	puts(command);
 //	for(int i=0;i<(sizeof(commands)/sizeof(char));i++){	
 //Need better if checks
 		if( /*(strcmp(command,commands[i]) == 0) &&*/ (strcmp(command,"cd") == 0)){
-				//Need to check if this is really required 
-			chdir_1(args);	
+			/*	if(args==NULL){	
+					puts("NULL");
+					args[0]="";
+				}	*/
+			//Need to check if this is really required 
+				chdir_1(args);	
 		}
 //	}
 }
@@ -83,19 +83,26 @@ void parseInput(){
 
 	temp = strtok(NULL," ");
 	while(temp!= NULL){
+		
 		args[i] = temp;
 		temp = strtok(NULL," ");
-	}
+//		puts(args[i]);
+		}
 }
 
 void chdir_1(char **args){
-//	puts("S");
+	//	puts(args[0]);
 		// Input validation for args need to be done
 		// Error cases to be handled	
-	
-	chdir(args[0]);	
-/*	int i = chdir(args[0]);
-	if(i==0){
-		puts("Success");
-	}*/
+	if(args[0] == NULL){
+		puts("No argument passed");
+		return;
+	}
+//	int i = chdir(args[0]);
+	if(chdir(args[0])!=0){
+		puts("Invalid CD usage. Check path and argument passed");
+	}
+	else{
+	puts("success");
+	}
 }
