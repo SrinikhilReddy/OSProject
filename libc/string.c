d2d0d98fe368b404de4507c84305e87d3dded66d
kavya2302 <ksivanesan@cs.stonybrook.edu>
2017-09-03 19:21:44 -0400

    strlen

--- libc/string.c
+++ libc/string.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 
-
-//lexicographic comparison
-int strcmp(char *s,char *t){
+int strcmp_(char *s,char *t){
 	while(*s==*t)
 	{
 		if(*s=='\0')
@@ -13,48 +11,7 @@ int strcmp(char *s,char *t){
 	return *s-*t;
 }
 
-char* strtok(char *string, const char *d){
-	
-	static char *s;
-	if(string!=NULL)
-	{
-		s=string;
-	}
-	static int index_s;
-	int index_d=0;
-	int i=index_s;
-	while(s[index_s]!='\0')
-	{	index_d=0;
-		while(d[index_d]!='\0')
-		{
-			if(s[index_s]==d[index_d])
-			{
-				s[index_s]='\0';
-			}
-			index_s++;	
-			if(s[i]!='\0')
-			{
-				return &s[i];
-			}
-			else
-			{
-				i=index_s;
-				index_s--;
-				break;
-			}	
-			index_d++;
-		}
-		index_s++;
-	}
-	s[index_s]='\0';
-	if(s[i]=='\0')
-		return NULL;
-	else
-		return &s[i];
-	
-}
-
-void strcpy(char *string2, char *string1){
+void strcpy_(char *string2, char *string1){
 
 	while(*string1)
 	{
@@ -64,3 +21,16 @@ void strcpy(char *string2, char *string1){
 	}
 	*string2='\0';
 }
+
+int strlen_(char *string)
+{
+	int length=0;
+	while(*string)
+	{
+		length++;
+		*string++;
+	}
+	return length;
+}
+
+
