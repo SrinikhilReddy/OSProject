b30f8c01a0401b48ea51c8f097c6010496a641a1
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-04 18:39:58 -0400

    execvp

--- libc/execvp.c
+++ libc/execvp.c
@@ -1,6 +1,6 @@
 #include "../include/syscall.h"
 int execvp(const char *file, char *const argv[]){
-	return _syscall2(char* ,execve,char*,&file,char**, argv);
+	return _syscall2(int,execve,char*,&file,char**, argv);
 }
 void main(){
 	char* ag[3]={"mkdir","rose",""};
