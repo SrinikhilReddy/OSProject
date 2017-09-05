e8177c0a22955e2d2b50cb6f5db978c9846b64d1
kavya2302 <ksivanesan@cs.stonybrook.edu>
2017-09-04 21:12:07 -0400

    few bug fixes

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "../../libc/string.c"
 #include <unistd.h>
 #include <sys/wait.h>
 
--- include/CONSTANTS.h
+++ include/CONSTANTS.h
@@ -2,3 +2,4 @@
 #define stdin 0
 #define stdout 1
 #define NULL 0
+#define O_RDONLY 00
