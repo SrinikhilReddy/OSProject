40c935546af70bdc00ca9ae150677d8d972019b4
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-05 17:16:12 -0400

    Fixes

--- libc/close.c
+++ libc/close.c
@@ -1,6 +1,10 @@
 #include "../include/syscall.h"
-
+#include <stdio.h>
 int close(int fd)
 {
 	_syscall1(int, close, int, fd);
+}
+int fclose(FILE* fp)
+{
+	return 	close(fp->fd);
 }
--- libc/execvp.c
+++ libc/execvp.c
@@ -2,5 +2,5 @@
 #include "putchar.c"
 int execvp(const char *file, char *const argv[]){
 //	  putchar(file[0]);
-	  _syscall2(int,execve,char*, file,char*,argv);
+	  _syscall3(int,execve,char*, file,char*,argv,char*, NULL);
 }
