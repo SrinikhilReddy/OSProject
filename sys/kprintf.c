7b9d23249bf556cc0296b35fb816aa95fcd443eb
nnaravamakul <nnaravamakul@cs.stonybrook.edu>
2017-09-11 22:18:17 -0400

    kprintf changes

--- sys/kprintf.c
+++ sys/kprintf.c
@@ -1,5 +1,59 @@
-#include <sys/kprintf.h>
+//#include <sys/kprintf.h>
+#include <stdarg.h>
+va_list list;
+void parse_str(const char* fmt);
+void kprintf(const char* fm, ...);
+char* print_int(register char* no,int i);
 
+char* print_int(register char* no,int i){
+	int j = 1000000000;
+	int lead_zero = 1;
+	if(i<0){
+		*no = '-';
+		no+=2;
+		i = i* -1;
+	}
+	while(j!=0){
+		if(lead_zero == 1 && (i/j == 0)){
+			j = j/10;
+			continue;
+		}
+		if(lead_zero == 1 && (i/j != 0)){
+			lead_zero = 0;	
+		}
+		*no = (char)('0'+(i/j));
+		i = i % j;
+		j = j/10;
+		no +=2;
+	}
+	return no;
+}
 void kprintf(const char *fmt, ...)
 {
+	va_start(list,fmt);
+	//print_val = (char*)0xb8000;
+	parse_str(fmt);
 }
+
+void parse_str(const char* s){
+	int i;
+	register char *print_val;
+	print_val = (char*)0xb8000;
+	while(*s!='\0'){
+	if(*s == '%'){
+		switch(*(s+1)){
+			case 'd':
+				i = va_arg(list,int);
+				print_val = (char*)print_int(print_val,va_arg(list,int));
+				s = s+2;
+				break;	
+		}
+	}
+	else{
+	   *print_val = *s;
+	   s++;
+	   print_val+=2;
+	}
+	}
+}
+
--- sys/main.c
+++ sys/main.c
@@ -11,18 +11,21 @@ extern char kernmem, physbase;
 
 void start(uint32_t *modulep, void *physbase, void *physfree)
 {
-  struct smap_t {
+	int i =10;
+	kprintf("A %d B",i);
+/*  struct smap_t {
     uint64_t base, length;
     uint32_t type;
   }__attribute__((packed)) *smap;
   while(modulep[0] != 0x9001) modulep += modulep[1]+2;
   for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
-    if (smap->type == 1 /* memory */ && smap->length != 0) {
+    if (smap->type == 1  && smap->length != 0) {
       kprintf("Available Physical Memory [%p-%p]\n", smap->base, smap->base + smap->length);
     }
   }
   kprintf("physfree %p\n", (uint64_t)physfree);
   kprintf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
+*/
 }
 
 void boot(void)
@@ -44,10 +47,10 @@ void boot(void)
     (uint64_t*)&physbase,
     (uint64_t*)(uint64_t)loader_stack[4]
   );
-  for(
+/*  for(
     temp1 = "!!!!! start() returned !!!!!", temp2 = (char*)0xb8000;
     *temp1;
     temp1 += 1, temp2 += 2
-  ) *temp2 = *temp1;
+  ) *temp2 = *temp1;*/
   while(1);
 }
