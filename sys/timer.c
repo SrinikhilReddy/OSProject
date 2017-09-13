5ec1c5d89ad90b28d27f8cc186208d87a0b2f5d8
kavya2302 <ksivanesan@cs.stonybrook.edu>
2017-09-12 20:58:39 -0400

    Basic timer logic working

---
+++ sys/timer.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdbool.h>
+#include<unistd.h>
+#include<signal.h>
+
+volatile sig_atomic_t flag = false;
+
+void handler(int s)
+{
+	flag = true;
+}
+
+int main()
+{
+
+	signal(SIGALRM, handler);
+	alarm(1);
+	
+	int i=0;	
+
+	/*time_t start;
+	struct tm *t;
+	time(&start);
+	t = localtime(&start);*/
+	while(1)
+	{
+		if(flag)
+		{
+			printf("%d\n",i);
+			++i;
+			flag = false;
+			alarm(1);
+			fflush(stdout);
+		}
+		/*printf("%d",t->tm_hour);
+		printf(":");
+		printf("%d",t->tm_min);
+		printf(":");
+		printf("%d",t->tm_sec);*/
+	}
+	//return 0;
+}
+
