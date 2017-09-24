64b15df26c1c95ee6660ec0c8504f4404785c204
kavya2302 <ksivanesan@cs.stonybrook.edu>
2017-09-24 16:30:16 -0400

    read function

--- sys/ahci.c
+++ sys/ahci.c
@@ -66,3 +66,91 @@ static int check_type(hba_port_t *port)
 		return AHCI_DEV_SATA;
 	}
 }
+
+BOOL read(HBA_PORT *port, DWORD startl, DWORD starth, DWORD count, WORD *buf)
+{
+	port->is = (DWORD)-1;		// Clear pending interrupt bits
+	int spin = 0; // Spin lock timeout counter
+	int slot = find_cmdslot(port);
+	if (slot == -1)
+		return FALSE;
+ 
+	HBA_CMD_HEADER *cmdheader = (HBA_CMD_HEADER*)port->clb;
+	cmdheader += slot;
+	cmdheader->cfl = sizeof(FIS_REG_H2D)/sizeof(DWORD);	// Command FIS size
+	cmdheader->w = 0;		// Read from device
+	cmdheader->prdtl = (WORD)((count-1)>>4) + 1;	// PRDT entries count
+ 
+	HBA_CMD_TBL *cmdtbl = (HBA_CMD_TBL*)(cmdheader->ctba);
+	memset(cmdtbl, 0, sizeof(HBA_CMD_TBL) +
+ 		(cmdheader->prdtl-1)*sizeof(HBA_PRDT_ENTRY));
+ 
+	// 8K bytes (16 sectors) per PRDT
+	for (int i=0; i<cmdheader->prdtl-1; i++)
+	{
+		cmdtbl->prdt_entry[i].dba = (DWORD)buf;
+		cmdtbl->prdt_entry[i].dbc = 8*1024;	// 8K bytes
+		cmdtbl->prdt_entry[i].i = 1;
+		buf += 4*1024;	// 4K words
+		count -= 16;	// 16 sectors
+	}
+	// Last entry
+	cmdtbl->prdt_entry[i].dba = (DWORD)buf;
+	cmdtbl->prdt_entry[i].dbc = count<<9;	// 512 bytes per sector
+	cmdtbl->prdt_entry[i].i = 1;
+ 
+	// Setup command
+	FIS_REG_H2D *cmdfis = (FIS_REG_H2D*)(&cmdtbl->cfis);
+ 
+	cmdfis->fis_type = FIS_TYPE_REG_H2D;
+	cmdfis->c = 1;	// Command
+	cmdfis->command = ATA_CMD_READ_DMA_EX;
+ 
+	cmdfis->lba0 = (BYTE)startl;
+	cmdfis->lba1 = (BYTE)(startl>>8);
+	cmdfis->lba2 = (BYTE)(startl>>16);
+	cmdfis->device = 1<<6;	// LBA mode
+ 
+	cmdfis->lba3 = (BYTE)(startl>>24);
+	cmdfis->lba4 = (BYTE)starth;
+	cmdfis->lba5 = (BYTE)(starth>>8);
+ 
+	cmdfis->countl = LOBYTE(count);
+	cmdfis->counth = HIBYTE(count);
+ 
+	// The below loop waits until the port is no longer busy before issuing a new command
+	while ((port->tfd & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000)
+	{
+		spin++;
+	}
+	if (spin == 1000000)
+	{
+		trace_ahci("Port is hung\n");
+		return FALSE;
+	}
+ 
+	port->ci = 1<<slot;	// Issue command
+ 
+	// Wait for completion
+	while (1)
+	{
+		// In some longer duration reads, it may be helpful to spin on the DPS bit 
+		// in the PxIS port field as well (1 << 5)
+		if ((port->ci & (1<<slot)) == 0) 
+			break;
+		if (port->is & HBA_PxIS_TFES)	// Task file error
+		{
+			trace_ahci("Read disk error\n");
+			return FALSE;
+		}
+	}
+ 
+	// Check again
+	if (port->is & HBA_PxIS_TFES)
+	{
+		trace_ahci("Read disk error\n");
+		return FALSE;
+	}
+ 
+	return TRUE;
+}
